52df83cf8d9deb677136a60f8cdd3932354287e8
caicailv <1401394657@qq.com>
2021-11-15 18:22:25 +0800

    '搭建环境'

--- lib/index.ts
+++ lib/index.ts
@@ -1,3 +1,11 @@
-export default function axios (){
-  console.log('axios.init');  
+export default function axios(url, params) {
+  return new Promise((resolve, reject) => {
+    const request = new XMLHttpRequest()
+    request.onreadystatechange = function () {
+      if (!request || request.readyState !== 4) return
+    }
+    request.open('POST', url, true)
+    request.setRequestHeader('Content-type', 'application/json')
+    request.send()
+  })
 }
